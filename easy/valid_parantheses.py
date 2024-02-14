6ca09edc2f1674a56773a0487b52fb56b73eb2ca
Sriram Natarajan <sriramnat123@gmail.com>
2024-02-14 08:51:57 -0800

    Update valid_parantheses.py

--- easy/valid_parantheses.py
+++ easy/valid_parantheses.py
@@ -1,13 +1,43 @@
-s = "()[]{}"
+class Solution(object):
+    def isValid(self, s):
+        s = list(s)
+        x = []
+        for i in s:
+            x.append(i)
+        
+        count = 0
 
-for i in range(len(s)):
-    if i == len(s)-1:
-        break
+        stack = []
+        hash_map = {"(": ")", 
+                "{": "}", 
+                "[": "]"}
 
-    if (s[i] == "(" and s[i+1] != ")") or (s[i] == ")" and s[i-1] != "("):
-        print("Nope")
-    elif (s[i] == "[" and s[i+1] != "]") or (s[i] == "]" and s[i-1] != "["):
-        print("Nope")
-    elif (s[i] == "{" and s[i+1] != "}") or (s[i] == "}" and s[i-1] != "{"):
-        print("Nope")
-    
+        if len(s) <= 1:
+            return False
+
+        for counter, i in enumerate(s):
+            if i in hash_map:
+                stack.append(i)
+                x.pop(counter - count)
+                count = count + 1
+
+            elif i in hash_map.values():
+                
+                if len(stack) > 0:
+                    n = stack[-1]
+                    if hash_map[n] == i:
+                        stack.pop(-1)
+                        x.pop(counter - count)
+                        count = count + 1
+                        
+
+                    else:
+                        return False
+            
+
+        if len(stack) != 0:
+            return False
+        elif len(x) != 0:
+            return False
+        else:
+            return True
